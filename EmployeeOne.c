545d146d511665f5404122aa57af5d52b1c372a0
Adrian Firo <afiro@cpp.edu>
2026-03-20 11:46:45 -0700

    Implemented search function for employee salary. Still needs test for match and no match.

--- EmployeeOne.c
+++ EmployeeOne.c
@@ -45,10 +45,15 @@ PtrToEmployee searchEmployeeByPhone(PtrToConstEmployee ptr, int tableSize, const
        return NULL; //no match found
 }
 PtrToEmployee searchEmployeeBySalary(PtrToConstEmployee ptr, int tableSize, float targetSalary); {
+    const PtrToConstEmployee endPtr = ptr + tableSize;
+
     //loop through the table
+    for (; ptr < endPtr; ptr++) {
 
-        //compare salary 
-        //return pointer if match found
+        if (strcmp(ptr->phone, targetSalary) == 0) { //compare salary 
+            return(PtrToEmployee)ptr; //return pointer if match found
+        }
+    }
 
-        //no match
+        return NULL; //no match
 }
